96b0a36e6244128d2d7b8e6b487fa78bcaba2343
RuchirDixit <ruchirtd96@gmail.com>
2020-12-02 14:31:49 +0530

    [Ruchir]Added: Database class to add data into database

---
+++ src/main/scala/com/bridgelabz/greetingapp/DbConfig.scala
@@ -0,0 +1,30 @@
+package com.bridgelabz.greetingapp
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.http.scaladsl.server.Directives
+import scala.concurrent.Future
+import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+
+object DbConfig extends App with Directives with MyJsonProtocol with MyXMLSupport {
+  val host = "0.0.0.0"
+  val port = 9000
+  implicit val system = ActorSystem("HelloWorld")
+  implicit val executor: ExecutionContext = system.dispatcher
+  val mongoClient: MongoClient = if (args.isEmpty) MongoClient() else MongoClient(args.head)
+  val database: MongoDatabase = mongoClient.getDatabase("mydb")
+  val collection: MongoCollection[Document] = database.getCollection("test")
+  collection.drop()
+
+  def sendRequest(greet: Greeting) = {
+    val doc: Document = Document("msg" -> greet.msg, "name" -> greet.name)
+    val bindFuture = collection.insertOne(doc).toFuture()
+    bindFuture.onComplete {
+      case Success(_) => println("Successfully Added!")
+      case Failure(exception) => println(exception)
+    }
+    Future{Done}
+  }
+}
