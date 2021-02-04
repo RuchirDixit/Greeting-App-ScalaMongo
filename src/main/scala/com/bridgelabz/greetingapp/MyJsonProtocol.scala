25b211bc131fdb923878d2c67fc2027489e8babe
RuchirDixit <ruchirtd96@gmail.com>
2021-02-04 11:33:41 +0530

    [Ruchir]Added: Separate packages

--- src/main/scala/com/bridgelabz/greetingapp/MyJsonProtocol.scala
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.greetingapp
-
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json.DefaultJsonProtocol
-
-// To convert object into json format
-trait MyJsonProtocol extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val templateFormat = jsonFormat2(Greeting)
-}
