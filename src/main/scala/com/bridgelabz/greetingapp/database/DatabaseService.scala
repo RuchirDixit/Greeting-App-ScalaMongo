139034fecb63e6625d43311e4548b701c15f9704
RuchirDixit <ruchirtd96@gmail.com>
2021-02-06 00:58:15 +0530

    [Ruchir]Added: Comments for methods

--- src/main/scala/com/bridgelabz/greetingapp/database/DatabaseService.scala
+++ src/main/scala/com/bridgelabz/greetingapp/database/DatabaseService.scala
@@ -1,9 +1,30 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.bridgelabz.greetingapp.database
 
+import akka.Done
+import com.bridgelabz.greetingapp.Routes.system
+import com.bridgelabz.greetingapp.caseclasses.Greeting
+import com.typesafe.scalalogging.LazyLogging
+import org.mongodb.scala.bson.collection.immutable.Document
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext
 object DatabaseService extends LazyLogging {
+  implicit val executor: ExecutionContext = system.dispatcher
   /**
    * Adds message and name to database and Future[Done]
    * @param greet : Data to be added into database
@@ -11,8 +32,8 @@ object DatabaseService extends LazyLogging {
    */
   def sendRequest(greet: Greeting) : Future[Done.type] = {
     logger.info("Sending request with object: " + greet)
-    val doc: Document = Document("msg" -> greet.msg, "name" -> greet.name)
-    val bindFuture = DbConfig.collection.insertOne(doc).toFuture()
+    val messageToBeAdded: Document = Document("msg" -> greet.msg, "name" -> greet.name)
+    val bindFuture = MongoDAL.collection.insertOne(messageToBeAdded).toFuture()
     bindFuture.onComplete {
       case Success(_) => logger.info("Successfully Added!")
       case Failure(exception) => logger.error(exception.toString)
@@ -20,9 +41,12 @@ object DatabaseService extends LazyLogging {
     Future{Done}
   }
 
-  // returns data in form of Future from mongodb database
+  /**
+   * Fetches all the messages in Json format
+   * @return : Future[Seq[Document]]
+   */
   def getJson() : Future[Seq[Document]] = {
-    val future = collection.find().toFuture()
+    val future = MongoDAL.collection.find().toFuture()
     future
   }
 }
