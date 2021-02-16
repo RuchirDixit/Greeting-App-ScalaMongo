eff1a900742bd8e52fe09ff14928d5546cfd1cf1
RuchirDixit <ruchirtd96@gmail.com>
2021-02-16 11:45:32 +0530

    [Ruchir]Added: User management routes to handle routes

---
+++ src/main/scala/com/bridgelabz/greetingapp/routes/UserManagementRoutes.scala
@@ -0,0 +1,86 @@
+
+package com.bridgelabz.greetingapp.routes
+
+import akka.Done
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import akka.http.scaladsl.server.{Directives, ExceptionHandler, Route}
+import com.bridgelabz.greetingapp.caseclasses.{Greeting, MyJsonProtocol}
+import com.bridgelabz.greetingapp.database.{DatabaseService, MongoDAL}
+import com.thoughtworks.xstream.XStream
+import com.thoughtworks.xstream.io.xml.DomDriver
+import com.typesafe.scalalogging.LazyLogging
+import scala.concurrent.{Future}
+import scala.util.{Success}
+
+class UserManagementRoutes extends LazyLogging with Directives with MyJsonProtocol{
+  // Handling Arithmetic and Null Pointer Exceptions
+  //$COVERAGE-OFF$
+  val myExceptionHandler = ExceptionHandler {
+    case _: ArithmeticException =>
+      extractUri { uri =>
+        logger.error(s"Request to $uri could not be handled normally")
+        complete(HttpResponse(StatusCodes.BadRequest, entity = "Bad numbers, bad result!!!"))
+      }
+    case _: NullPointerException =>
+      extractUri { uri =>
+        logger.error(s"Request to $uri could not be handled normally")
+        complete(HttpResponse(StatusCodes.BadRequest, entity = "Null value found!!!"))
+      }
+    case exception: Exception =>
+      extractUri { uri =>
+        logger.error(s"Request to $uri could not be handled normally")
+        complete(HttpResponse(StatusCodes.BadRequest, entity = exception.toString))
+      }
+  }
+  def route : Route =
+    handleExceptions(myExceptionHandler){
+      concat(
+        /**
+         * for saving messages and name
+         * @input : It accepts message and name from body
+         * @Return :  Data inserted on successful insertion
+         */
+        post {
+          path("message") {
+            entity(as[Greeting]) {
+              emp =>
+                val request: Future[Done] = DatabaseService.sendRequest(emp)
+                onComplete(request) {
+                  _ =>
+                    logger.info("Data inserted")
+                    complete("Data Inserted!")
+                }
+            }
+          }
+        },
+        /**
+         * for getting messages in JSON format
+         * @input : Fetches all the messages saved
+         * @Return : Displays all the messages in json format
+         */
+        get {
+          concat(
+            path("getJson") {
+              val greetingSeqFuture: Future[Seq[Greeting]] = MongoDAL.fetchAllGreetings()
+              complete(greetingSeqFuture)
+            },
+            /**
+             * for getting messages in XML format
+             * @input : Fetches all the messages saved
+             * @Return : Displays all the messages in XML format
+             */
+            path("getXML"){
+              val greetingSeqFuture = MongoDAL.fetchAllGreetings()
+              onComplete(greetingSeqFuture) {
+                case Success(data) =>
+                  val xStream = new XStream(new DomDriver())
+                  val xml = xStream.toXML(data)
+                  logger.info("XML data:" + xml)
+                  complete(xml)
+              }
+            }
+          )
+        }
+      )
+    }
+}
